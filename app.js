d24b8476402c995030598818c08fd0a0bd6f14e9
Borismildov <bokataand1@abv.bg>
2019-05-01 11:30:45 +0300

    test commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express(); // the express package seems to export a function in the 
 // app constant here
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
-
+console.log("test git");
+console.log();
 app.use(bodyParser.urlencoded({extended: false})); // I want to parce the incomming request body; t.e da vzimam informaciqta ot formite
 
 app.use(adminRoutes); //the exporting file. the order matters so if we put this middleware below, we will never reach that
